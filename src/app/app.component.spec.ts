a0253f80ff839e260b2387c4aa7f59fd113ea445
Jeffrey Arias <jeffreyagnz@Jeffreys-MacBook-Pro.local>
2018-07-19 09:27:16 -0400

    Added Template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to bb!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
+
+
---
+++ src/app/app.routing.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+
+import { FullComponent } from './layouts/full/full.component';
+import { LoginComponent } from './login/login.component';
+
+export const AppRoutes: Routes = [{
+  path: 'dashboard',
+  component: FullComponent,
+  children: [{ 
+    path: '', 
+    redirectTo: '/starter', 
+    pathMatch: 'full' 
+  }, {
+    path: '',
+    loadChildren: './material-component/material.module#MaterialComponentsModule'
+  }, {
+    path: 'starter',
+    loadChildren: './starter/starter.module#StarterModule'
+  }]
+}, {
+  path: '',
+  component: LoginComponent,
+}];
+
