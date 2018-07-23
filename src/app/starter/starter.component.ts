c1b8d0e4bb73224473573c4e9a749b2c87d28410
Jeffrey Arias <jeffreyagnz@Jeffreys-MacBook-Pro.local>
2018-07-23 16:25:44 -0400

    Changed template color & style

--- src/app/starter/starter.component.ts
+++ src/app/starter/starter.component.ts
@@ -3,7 +3,7 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import {FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'starter',
+  selector: '/starter',
   templateUrl: './starter.component.html',
   styleUrls: ['./starter.component.scss']
 })
